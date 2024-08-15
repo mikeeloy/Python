a61293d9e2f86e3ab88864aae3cf8d1c275c3e60
mikeeloy <mikeeloy.me@gmail.com>
2024-08-15 18:50:18 -0400

    python

--- Hi.everyone.py
+++ Hi.everyone.py
@@ -2,3 +2,7 @@ print('hello,world2')
 print("I'm here")
 print('I\m trying')
 print('my name is Michael ')
+
+height_cm = input('Height converter: enter your height in cm: ')
+float_height_cm = float(height_cm)
+print('Your height in feet is:', float_height_cm / 30.48 )
